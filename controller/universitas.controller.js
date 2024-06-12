be3c21110324a82631259707e71ddef87d5dd227
klnkvc <klnkvc@gmail.com>
2024-06-12 14:39:52 +0700

    delete univ

--- controller/universitas.controller.js
+++ controller/universitas.controller.js
@@ -1,11 +1,20 @@
 const query = require("../database");
 const getAllUniversitas= async (req,res)=>{
     try {
-        const result = await query("SELECT id_university, university_name, email, address, pic_name, pic_phone, pic_email FROM university;\n")
+        const result = await query("SELECT id_university as id, university_name, email, address, pic_name, pic_phone, pic_email FROM university;\n")
         return res.status(200).json({data:result})
     }catch (e) {
         return res.status(400).json({msg:"Something Wrong", error:e})
     }
 }
+const deleteUniversitas = async (req,res)=>{
+    const {id} = req.params
+    try{
+        await query("DELETE FROM university WHERE id_university=?", [id])
+        return res.status(200).json({msg:"Delete Success"})
+    }catch (e) {
+        return res.status(400).json({msg:"Something Wrong", error:e})
+    }
+}
 
-module.exports = {getAllUniversitas}
+module.exports = {getAllUniversitas, deleteUniversitas}
--- route/universitas.route.js
+++ route/universitas.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {getAllUniversitas} = require("../controller/universitas.controller");
+const {getAllUniversitas, deleteUniversitas} = require("../controller/universitas.controller");
 const routes = express()
 
 routes.get('/universitas', getAllUniversitas)
+routes.delete('/universitas/:id', deleteUniversitas)
 
 module.exports=routes
 
