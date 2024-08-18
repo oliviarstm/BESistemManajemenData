2ddcf004e4b7556274d6d9c0ff7e9886bccae859
klnkvc <klnkvc@gmail.com>
2024-08-18 23:48:28 +0700

    University is Functional

--- controller/universitas.controller.js
+++ controller/universitas.controller.js
@@ -1,8 +1,42 @@
 const query = require("../database");
 const getAllUniversitas= async (req,res)=>{
     try {
-        const result = await query("SELECT id_university as id, university_name, email, address, pic_name, pic_phone, pic_email FROM university;\n")
+        const result = await query("SELECT id_university as id, university_name, email, address, pic_name, pic_phone, pic_email FROM university")
         return res.status(200).json({data:result})
+    }catch (e) {
+        return res.status(400).json({msg:"Something Wrong", error:e})
+    }
+}
+const getUniversitasById=async (req,res)=>{
+    const {id} = req.params
+    try {
+        if (id===undefined||id===''){
+            return res.status(400).json({msg:"Field cant be empty"})
+        }
+        const [result]= await query("SELECT id_university as id, university_name, email, address, pic_name, pic_phone, pic_email FROM university WHERE id_university=?", [id])
+        return res.status(200).json({data:result})
+    }catch (e) {
+        return res.status(400).json({msg:"Something Wrong", error:e})
+    }
+}
+const createUniversitas=async (req,res)=>{
+    const {university_name, email, address, pic_name, pic_phone, pic_email} = req.body
+    try {
+        await query("INSERT INTO university(university_name, email, address, pic_name, pic_phone, pic_email) VALUES (?,?,?,?,?,?)", [university_name,email,address,pic_name,pic_phone,pic_email])
+        return res.status(200).json({msg:"Universitas Created"})
+    }catch (e) {
+        return res.status(400).json({msg:"Something Wrong", error:e})
+    }
+}
+const updateUniversitas=async (req,res)=>{
+    const {id} = req.params
+    const {university_name, email, address, pic_name, pic_phone, pic_email} = req.body
+    if (id===undefined||id===''){
+        return res.status(400).json({msg:"Field cant be empty"})
+    }
+    try {
+        await query("UPDATE university SET university_name=?, email=?, address=?, pic_name=?, pic_phone=?, pic_email=? WHERE id_university=?", [university_name,email,address,pic_name,pic_phone,pic_email,id])
+        return res.status(200).json({msg:"Universitas Updated"})
     }catch (e) {
         return res.status(400).json({msg:"Something Wrong", error:e})
     }
@@ -17,4 +51,4 @@ const deleteUniversitas = async (req,res)=>{
     }
 }
 
-module.exports = {getAllUniversitas, deleteUniversitas}
+module.exports = {getAllUniversitas, deleteUniversitas, getUniversitasById, createUniversitas, updateUniversitas}
--- route/universitas.route.js
+++ route/universitas.route.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const {getAllUniversitas, deleteUniversitas} = require("../controller/universitas.controller");
+const {getAllUniversitas, deleteUniversitas, getUniversitasById, createUniversitas, updateUniversitas} = require("../controller/universitas.controller");
 const routes = express()
 
 routes.get('/universitas', getAllUniversitas)
+routes.get('/universitas/:id', getUniversitasById)
+routes.post('/universitas', createUniversitas)
+routes.put('/universitas/:id', updateUniversitas)
 routes.delete('/universitas/:id', deleteUniversitas)
 
 module.exports=routes
